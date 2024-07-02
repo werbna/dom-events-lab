df9a24ffa75159fb568b47128b7aa0bc43732b48
werbnah <andrewchau1993@gmail.com>
2024-07-02 13:58:41 -0700

    finished everything as you requested :3

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 /*-------------------------------- Constants --------------------------------*/
 
 /*-------------------------------- Variables --------------------------------*/
+let currentInput = "";
+const buttons = document.querySelectorAll(".button");
+const display = document.querySelector(".display");
 
 /*------------------------ Cached Element References ------------------------*/
 
 /*----------------------------- Event Listeners -----------------------------*/
+buttons.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    const value = event.target.innerText;
+
+    if (value === "C") {
+      currentInput = "";
+      display.innerText = "";
+    } else if (value === "=") {
+      try {
+        currentInput = eval(currentInput).toString();
+        display.innerText = currentInput;
+      } catch (error) {
+        display.innerText = "Error";
+        currentInput = "";
+      }
+    } else {
+      currentInput += value;
+      display.innerText = currentInput;
+    }
+  });
+});
 
 /*-------------------------------- Functions --------------------------------*/
